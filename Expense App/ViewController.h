6c61c8983434f33bb3be6b41ea91f0a9814a5986
dtoulso1 <drtoulson@gmail.com>
2015-09-15 14:10:52 -0400

    Initial Commit

---
+++ Expense App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Expense App
+//
+//  Created by Donald Toulson on 9/15/15.
+//  Copyright (c) 2015 Donald Toulson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Expense App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Expense App
+//
+//  Created by Donald Toulson on 9/15/15.
+//  Copyright (c) 2015 Donald Toulson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
